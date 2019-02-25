9b0b9dad016f49c9b8ef4dc46d674b48905c8d3c
Shwuup <e_kong98z@hotmail.com>
2019-02-25 18:13:26 +1100

    add table for books

---
+++ backend/bookshelf/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    book_cover_path = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
